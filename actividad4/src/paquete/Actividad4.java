6553631da033138c530d8305d28753fa46d67931
aperbad0812 <aperbad0812@g.educaand.es>
2024-04-16 14:27:00 +0200

    Cambios realiazados en alumnos

--- actividad4/src/paquete/Actividad4.java
+++ actividad4/src/paquete/Actividad4.java
@@ -10,15 +10,35 @@ public class Actividad4 {
 		Asignatura nombreM = new Asignatura(2);
 		Asignatura nombreS = new Asignatura(3);
 		
+		
+		
+	
+	
 		Alumno paco = new Alumno(nombreL,nombreM,nombreS);
 		
-		
 		Profesor javi = new Profesor();
+		
+		
+		Alumno alex = new Alumno("Juan", 30, "12345678Z",nombreL,nombreM,nombreS);
+		javi.ponerNotas(alex);
+		System.out.println(alex.toStringAlumno());
+   
+		
+		
+		
 		
 		javi.ponerNotas(paco);
 		
 		System.out.println(String.format("La Nota media: %.2f", javi.calcularMedia(paco)));
 		System.out.println( "La Nota de lengua:" + paco.getLengua().getCalificacion());
+	
+		
+	
+		
+		
+	
+	
+	
 	}
 
 }
--- actividad4/src/paquete/Alumno.java
+++ actividad4/src/paquete/Alumno.java
@@ -6,43 +6,75 @@ public class Alumno {
 	private Asignatura lengua;
 	private Asignatura mates;
 	private Asignatura sociales;
+	private String nombre;
+	private int edad;
+	private String dni;
 	
 	
 	
 	
 	
-	public Alumno(Asignatura lengua, Asignatura mates, Asignatura sociales) {
+	public Alumno (Asignatura lengua, Asignatura mates, Asignatura sociales) {
 		
+
 		this.lengua = lengua;
 		this.mates = mates;
 		this.sociales = sociales;
 	}
 
-	public Alumno(int lengua, int  mates, int sociales) {
+	public Alumno (int lengua, int  mates, int sociales) {
+		
+		
 		
 		this.lengua = new Asignatura(lengua);
 		this.mates = new Asignatura(mates);
 		this.sociales = new Asignatura(sociales);
 	}
 
+	
+	public Alumno(String nombre, int edad, String dni,Asignatura lengua, Asignatura mates, Asignatura sociales) {
+		
+		  this.nombre = nombre;
+	      this.edad = edad;
+	      this.dni = dni;
+	      this.lengua = lengua;
+		  this.mates = mates;
+		  this.sociales = sociales;
+	}
+		
+		
+	public String getNombre() {
+		return nombre;
+	}
 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
+	public int getEdad() {
+		return edad;
+	}
 
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
 
 	public Asignatura getLengua() {
 		return lengua;
 	}
 
-
-
-
-
+	
 	public Asignatura getMates() {
 		return mates;
 	}
-
-
-
 
 
 	public Asignatura getSociales() {
@@ -51,6 +83,8 @@ public class Alumno {
 	
 	
 	
-	
+	public String toStringAlumno() {
+		return "Nombre: "+ nombre +" Edad: "+ edad +" DNI: "+ dni +"Clasificacion de las tres asignaturas: "+lengua.getCalificacion()+", "+sociales.getCalificacion()+", "+mates.getCalificacion()+".";
+	}
 
 }
